94e8710c170e96cdcdd3d3e69349add6bd6788c9
fzninja <497915773@qq.com>
2022-02-18 03:48:37 +0800

    update version 0.0.496

--- linux-install.sh
+++ linux-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="0.0.493"
+VERSION="0.0.496"
 TAR_URL="https://github.com/kt007007/KTMinerProxy-Linux/blob/master/KT-v${VERSION}-LINUX.tar.gz"
 SUPERVISOR_PATH="https://raw.githubusercontent.com/kt007007/KTMinerProxy/main/supervisord.conf"
 SUPERVISOR_D_PATH="https://raw.githubusercontent.com/kt007007/KTMinerProxy/main/ktproxy.conf"
@@ -74,7 +74,7 @@ install() {
     filterResult $? "安装python-setuptools"
     
     message "安装supervisor"
-    $cmd install supervisor -y
+    $cmd install supervisor -y 1>/dev/null
     filterResult $? "安装supervisor"
     
     message "创建目录"
