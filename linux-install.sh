8fe37269a3a5317439578d83f3b73f8e6775e366
fzninja <497915773@qq.com>
2022-02-17 10:07:55 +0800

    update version 0.0.4

--- linux-install.sh
+++ linux-install.sh
@@ -85,7 +85,9 @@ install() {
     cd ${KT_PATH}
     wget -P $KT_PATH $TAR_URL --no-check-certificate 1>/dev/null
     chmod 777 "${KT_PATH}/${KT_TAR_NAME}" 1>/dev/null
+    message "解压文件"
     tar -xf "${KT_PATH}/${KT_TAR_NAME}" -C "${KT_PATH}" 1>/dev/null
+    filterResult $? "拉取文件"
     chmod 777 "${KT_PATH}/${EXEC_NAME}" 1>/dev/null
     rm "${KT_PATH}/${KT_TAR_NAME}"
     if [ -f "${KT_PATH}/defend.sh" ];then
