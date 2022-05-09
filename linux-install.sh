cf67f7e9cb7f48cea69fdd5340009986fe949f50
fzninja <497915773@qq.com>
2022-05-09 22:27:49 +0800

    update version 2.2.0

--- linux-install.sh
+++ linux-install.sh
@@ -2,7 +2,7 @@
 # Author: KT007007
 # github: https://github.com/kt007007
 
-VERSION="2.1.1"
+VERSION="2.2.0"
 
 DOWNLOAD_HOST="https://github.com/kt007007/KTMinerProxy/raw/main/Linux-64"
 
@@ -32,6 +32,7 @@ ISSUE() {
     echo "2.0.0"
     echo "2.0.1"
     echo "2.1.0"
+    echo "2.1.1"
 }
 
 colorEcho(){
@@ -163,7 +164,7 @@ start() {
 }
 
 update() {
-    installapp 2.1.1
+    installapp 2.2.0
 }
 
 turn_on() {
@@ -403,7 +404,7 @@ read -p "$(echo -e "请选择[1-14]：")" choose
 
 case $choose in
 1)
-    installapp 2.1.1
+    installapp 2.2.0
     ;;
 2)
     uninstall
