00bc4d96de310706308b2b564ea699fa39e33a59
fzninja <497915773@qq.com>
2022-06-21 18:25:04 +0800

    update version 2.6.3

--- linux-install.sh
+++ linux-install.sh
@@ -2,7 +2,7 @@
 # Author: KT007007
 # github: https://github.com/kt007007
 
-VERSION="2.6.2"
+VERSION="2.6.3"
 
 DOWNLOAD_HOST="https://github.com/kt007007/KTMinerProxy/raw/main/Linux-64"
 
@@ -202,7 +202,7 @@ start() {
 update() {
     turn_off
 
-    installapp 2.6.2
+    installapp 2.6.3
 }
 
 turn_on() {
@@ -471,7 +471,7 @@ read -p "$(echo -e "请选择[1-14]：")" choose
 
 case $choose in
 1)
-    installapp 2.6.2
+    installapp 2.6.3
     ;;
 2)
     update
